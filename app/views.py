b34351e161679f99cdc10af7b90e993a27d49f51
sahithi5c7 <sahithipadmakar@gmail.com>
2023-12-01 21:18:45 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb1(request):
+    return render(request,'mdb1.html')
+
+def mdb2(request):
+    return render(request,'mdb2.html')
